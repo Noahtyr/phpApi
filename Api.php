174c60dc8870a55f1e5b70d564050d647a38511b
Juaquin <Noahdavidian@hotmail.dk>
2017-11-15 13:46:23 +0100

    Changed the PHP API file, Chaning the Switch Statement to an IF statement, since we only need GET and not the rest  of the Switch Block

--- Api.php
+++ Api.php
@@ -6,7 +6,7 @@
  * Time: 10:04
  */
 
-// get the HTTP method, path and body of the request
+//This is our instance Variable
 $method = $_SERVER['REQUEST_METHOD'];
 $array = [];
 parse_str ($_SERVER['QUERY_STRING'], $array);
@@ -19,33 +19,16 @@ mysqli_set_charset($link,'utf8');
 }
 
 // create SQL based on HTTP method
-switch ($method) {
-    case 'GET':
-        $sql = "select * from country WHERE Name =$key"; break;
+if ($method == 'GET' && array_key_exists('name',$array)) {
+    $sql = "Select * FROM country WHERE Name =".$array['name'];
+    $result = mysqli_query($link,$sql);
+    if ($result){
 
-}
-
-// excecute SQL statement
-$result = mysqli_query($link,$sql);
-
-// die if SQL statement failed
-if (!$result) {
-    http_response_code(404);
-    die(mysqli_error());
-}
-
-// print results, insert id or affected row count
-if ($method == 'GET') {
-    if (!$key) echo '[';
-    for ($i=0;$i<mysqli_num_rows($result);$i++) {
-        echo ($i>0?',':'').json_encode(mysqli_fetch_object($result));
     }
-    if (!$key) echo ']';
-} elseif ($method == 'POST') {
-    echo mysqli_insert_id($link);
-} else {
-    echo mysqli_affected_rows($link);
 }
+
+
+
 
 // close mysql connection
 mysqli_close($link);
