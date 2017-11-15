23cbdf98acb11924a9001b23cd3896240eab4323
Juaquin <Noahdavidian@hotmail.dk>
2017-11-15 13:35:05 +0100

    Changed the PHP API file, so we removed unnessecary code

--- Api.php
+++ Api.php
@@ -8,41 +8,21 @@
 
 // get the HTTP method, path and body of the request
 $method = $_SERVER['REQUEST_METHOD'];
-$request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
-$input = json_decode(file_get_contents('php://input'),true);
+$array = [];
+parse_str ($_SERVER['QUERY_STRING'], $array);
 
 // connect to the mysql database
 $link = mysqli_connect('localhost', 'root', 'root', 'world');
 mysqli_set_charset($link,'utf8');
 
-// retrieve the table and key from the path
-$table = preg_replace('/[^a-z0-9_]+/i','',array_shift($request));
-$key = array_shift($request)+0;
 
-// escape the columns and values from the input object
-$columns = preg_replace('/[^a-z0-9_]+/i','',array_keys($input));
-$values = array_map(function ($value) use ($link) {
-    if ($value===null) return null;
-    return mysqli_real_escape_string($link,(string)$value);
-},array_values($input));
-
-// build the SET part of the SQL command
-$set = '';
-for ($i=0;$i<count($columns);$i++) {
-    $set.=($i>0?',':'').'`'.$columns[$i].'`=';
-    $set.=($values[$i]===null?'NULL':'"'.$values[$i].'"');
 }
 
 // create SQL based on HTTP method
 switch ($method) {
     case 'GET':
         $sql = "select * from country WHERE Name =$key"; break;
-    case 'PUT':
-        $sql = "update `$table` set $set where id=$key"; break;
-    case 'POST':
-        $sql = "insert into `$table` set $set"; break;
-    case 'DELETE':
-        $sql = "delete `$table` where id=$key"; break;
+
 }
 
 // excecute SQL statement
@@ -69,3 +49,4 @@ if ($method == 'GET') {
 
 // close mysql connection
 mysqli_close($link);
+
